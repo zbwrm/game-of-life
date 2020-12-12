60a7dacd3123bba8ebdc917eb72fef88b93d9b33
Aydin O'Leary <aoleary@olin.edu>
2020-12-11 19:04:32 -0500

    Scaffold wrapper module

---
+++ vivado project/game_of_life.srcs/sources_1/new/wrapper.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+
+// havent touched verilog in a min, help w syntax?
+module WRAPPER
+    (input clk, _rst
+     output row, col);
+    
+    // HIGH should be 3.3V, not 5V for LED reasons 
+     
+    // row is the actual data of each row
+    // col is a one-hot encoding of the column that's being displayed this split second
+     
+    // PINS
+    // COLUMNS
+    //  JB rightmost 8 pins, counterclockwise (can be changed easily)
+    // ROWS
+    //  JC rightmost 8 pins, see above
+    
+    // slow down clock a bit
+        // pass slightly faster clock to row counter
+        // basically PWM, displaying one row at a time
+        // not toooo fast: dont wanna cycle LEDs too much? 
+        // test this
+    // pass slightly slower clock to GOL matrix so it updates, at most, 2 times/s
+    // pass GOL out to row
+        // intermediate matrix stage? probably won't need
+    
+    );
+endmodule
