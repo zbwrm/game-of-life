90fdd288b0534729f60649701ebdc1e02828e8a6
Aydin O'Leary <aoleary@olin.edu>
2020-12-15 13:55:31 -0500

    Try some stuff

--- verilog/is_2.v
+++ verilog/is_2.v
@@ -5,11 +5,17 @@ module IS_2(
   output reg Checked); 
   
   reg total;
-
-  assign total = Tl + T + Tr + L + R + Bl + B + Br;
   
-  always @ (total) begin
-      case (total)
+  assign total0 = Tl + T;
+  assign total1 = total0 + Tr;
+  assign total2 = total1 + L;
+  assign total3 = total2 + R;
+  assign total4 = total3 + Bl;
+  assign total5 = total4 + B;
+  assign total6 = total5 + Br;
+  
+  always @ (total6) begin
+      case (total6)
           2 : Checked = 1;
           default : Checked = 0;
       endcase
